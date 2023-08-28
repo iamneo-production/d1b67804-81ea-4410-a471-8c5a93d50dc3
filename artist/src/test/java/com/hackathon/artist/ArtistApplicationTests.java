7488c07e6b2b1c20d052300f3c87f36d2a1f937d
ashutoshsingh1814 <65407888+ashutoshsingh1814@users.noreply.github.com>
2023-08-28 12:28:40 +0530

    Updated ArtistApplicationTests.java

--- artist/src/test/java/com/hackathon/artist/ArtistApplicationTests.java
+++ artist/src/test/java/com/hackathon/artist/ArtistApplicationTests.java
@@ -2,9 +2,8 @@ package com.hackathon.artist;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-@SpringBootApplication
-@EnableDiscoveryClient
+
+@SpringBootTest
 class ArtistApplicationTests {
 
 	@Test
