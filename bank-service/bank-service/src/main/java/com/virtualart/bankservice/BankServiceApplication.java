98132fb9ca544d3e4ba0d56f9fe3908927edfb98
ashutoshsingh1814 <65407888+ashutoshsingh1814@users.noreply.github.com>
2023-08-28 12:31:52 +0530

    Updated BankServiceApplication.java

--- bank-service/bank-service/src/main/java/com/virtualart/bankservice/BankServiceApplication.java
+++ bank-service/bank-service/src/main/java/com/virtualart/bankservice/BankServiceApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableFeignClients
 public class BankServiceApplication {
 	public static void main(String[] args) {
